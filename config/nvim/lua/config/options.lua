f1061ff390415ae479ad0cd619982ca480723519
Mike Krisher <mike@mikekrisher.com>
2025-10-15 15:39:58 -0400

    Updates dotfiles to include luasnip

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,3 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.mapleader = "\\"
+
+-- Enable absolute and relative line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.statuscolumn = "%s %l %r "
